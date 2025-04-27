1785b3ad2cd378767aff8595bc10cb75f2faa33a
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-04-28 00:20:48 +0200

    feat: add vine librairie to benchmarks

--- bin/dartmark.dart
+++ bin/dartmark.dart
@@ -3,6 +3,7 @@ import 'package:dartmark/src/executor.dart';
 void main(List<String> arguments) {
   final Executor executor = Executor([
     'acanthis',
+    'vine',
   ]);
   executor.execute();
 }
