b487d642380a5c1b9e19d218e40d8871345277fb
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-06-28 19:02:09 +0200

    chore: disable vine because of an issue in the library and update acanthis version

--- bin/dartmark.dart
+++ bin/dartmark.dart
@@ -1,6 +1,6 @@
 import 'package:dartmark/src/executor.dart';
 
 void main(List<String> arguments) {
-  final Executor executor = Executor(['acanthis', 'luthor', 'vine']);
+  final Executor executor = Executor(['acanthis', 'luthor']);
   executor.execute();
 }
