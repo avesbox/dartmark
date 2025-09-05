70ada438dc44e598c3d22cc6f993bb506485655d
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-09-06 01:24:11 +0200

    feat: add vine

--- bin/dartmark.dart
+++ bin/dartmark.dart
@@ -1,6 +1,6 @@
 import 'package:dartmark/src/executor.dart';
 
 void main(List<String> arguments) {
-  final Executor executor = Executor(['acanthis', 'luthor', 'ack']);
+  final Executor executor = Executor(['acanthis', 'luthor', 'ack', 'vine']);
   executor.execute();
 }
