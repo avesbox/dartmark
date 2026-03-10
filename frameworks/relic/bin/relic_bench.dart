0686ee1fc48bff1f58430ff5c05e94e86eb7c875
Francesco Vallone <vallonefrancesco587@gmail.com>
2026-03-10 16:48:35 +0100

    fix: relic now is aligned to all the other frameworks

--- frameworks/relic/bin/relic_bench.dart
+++ frameworks/relic/bin/relic_bench.dart
@@ -6,12 +6,11 @@ Future<void> main(List<String> arguments) async {
   final app = RelicApp()..get(
       '/health', (req) => Response.ok(body: Body.fromString('ok')))..get(
       '/api/echo',
-      (req) => Response.ok(
-        body: Body.fromString(jsonEncode({
-          'message': 'Hello, World!',
-          'dateTime': DateTime.now().toIso8601String(),
-        })
-      ),
-    ));
+      (req) async {
+        final jsonBody = await req.readAsString();
+        final body = jsonDecode(jsonBody.isEmpty ? '{}' : jsonBody);
+        return Response.ok(body: Body.fromString(jsonEncode(body)));
+      }
+    );
   await app.serve(port: int.parse(Platform.environment['PORT'] ?? '8080'));
 }
