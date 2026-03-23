34971c9e40a788bbb6ce82646806b03d099cd520
Gustavo Guzman <guzmanlo.2001@gmail.com>
2026-03-23 20:46:58 +0100

    feat: add netto benchmarks

---
+++ frameworks/netto/bin/netto_bench.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:netto/netto.dart';
+
+Future<void> main(List<String> arguments) async {
+  final app = Netto()
+    ..get('/health', (ctx) => ctx.response.string("ok"))
+    ..get('/api/echo', (ctx) async {
+      final body = await ctx.request.body.json();
+      return ctx.response.json(jsonEncode(body));
+    });
+
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+
+  await app.serve(InternetAddress.anyIPv4, port);
+}
